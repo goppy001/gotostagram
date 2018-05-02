22c1cbed7efbbb707c4240df5b33dc78d8b1108a
glodia <glodia@localhost.localdomain>
2018-05-02 14:02:11 +0900

    【変更】ホーム画面のレイアウト変更とルーティングを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
-end
+
+  def create
+    @user  = User.new(user_params)
+    if @user.save
+      sign_in @user
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/home', to: "static_pages#home"
   get '/help', to: "static_pages#help"
   get 'signup', to: "users#new"
-
+  resources :users
 
 end
