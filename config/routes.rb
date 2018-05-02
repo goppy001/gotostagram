dc5d7106e41a47da322f139aaac2e45a68e41b8e
glodia <glodia@localhost.localdomain>
2018-05-02 15:28:27 +0900

    【完了】ユーザー登録ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get '/home', to: "static_pages#home"
-  get '/help', to: "static_pages#help"
-  get 'signup', to: "users#new"
-
-
+  get  'users/new'
+  get  '/home',   to: "static_pages#home"
+  get  '/help',   to: "static_pages#help"
+  get  '/signup', to: "users#new"
+  post '/signup', to: 'users#create'
+  resources :users
 end
