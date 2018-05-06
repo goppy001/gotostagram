6bc47e41d964c8f0583f1542212a0220ab8efdef
glodia <glodia@localhost.localdomain>
2018-05-07 08:52:51 +0900

    【ADD】ログイン・ログアウト実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get  'users/new'
-  get  '/home',   to: "static_pages#home"
-  get  '/help',   to: "static_pages#help"
-  get  '/signup', to: "users#new"
-  post '/signup', to: 'users#create'
+  root    'sessions#new'
+  get     '/home',    to: "static_pages#home"
+  get     '/help',    to: "static_pages#help"
+  get     '/signup',  to: "users#new"
+  get     '/login',   to: "sessions#new"
+  post    '/login',   to: "sessions#create"
+  delete  '/logout',  to: "sessions#destroy"
   resources :users
 end
